696225e2185bef48ca2d1b591f7334c3915c1402
J-Breno <joao.breno85@hotmail.com>
2025-07-11 17:07:22 -0300

    Links without full page reload

--- exercicios/tutorial-react-router/src/App.tsx
+++ exercicios/tutorial-react-router/src/App.tsx
@@ -1,11 +1,15 @@
 
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <nav>
+        <Link to="/expenses">Expenses</Link>
+        <Link to="/invoices">Invoices</Link> |{" "}
+      </nav>
     </>
   )
 }
