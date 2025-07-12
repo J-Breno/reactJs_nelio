497fb58f4f2fc664cf80bea244e0d743094c51b3
J-Breno <joao.breno85@hotmail.com>
2025-07-12 05:23:24 -0300

    Acessando parâmetros de URL

---
+++ exercicios/tutorial-react-router/src/routes/Invoices/Invoice/index.tsx
@@ -0,0 +1,27 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteInvoice, getInvoice } from "../../../data";
+
+export default function Invoice() {
+
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const invoice = getInvoice(Number(params.invoiceId));
+
+    return (
+        <>
+            {
+                invoice &&
+                <main style={{ padding: "1rem" }}>
+                    <h2>Total Due: {invoice.amount}</h2>
+                    <p>
+                        {invoice.name}: {invoice.number}
+                    </p>
+                    <p>Due Date: {invoice.due}</p>
+                    
+                </main>
+
+            }
+        </>
+    );
+}   
