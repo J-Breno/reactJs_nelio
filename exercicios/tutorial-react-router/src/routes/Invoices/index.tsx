9fa6a90f22d282b9d8695eec1bfc8b6e2f6fcf75
J-Breno <joao.breno85@hotmail.com>
2025-07-12 05:52:07 -0300

    Filtrando barra de navegação com parâmetros de busca

--- exercicios/tutorial-react-router/src/routes/Invoices/index.tsx
+++ exercicios/tutorial-react-router/src/routes/Invoices/index.tsx
@@ -1,31 +1,54 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
 import './styles.css';
+import QueryLink from "../../components/QueryLink";
 
 export default function Invoices() {
-  const invoices = getInvoices();
 
-  return (
-    <>
-      <div style={{ display: "flex" }}>
-        <nav
-          style={{
-            borderRight: "solid 1px",
-            padding: "1rem",
-          }}
-        >
-          {invoices.map((invoice) => (
-            <NavLink
-              className={({ isActive }) => (isActive ? "dblock nav-red" : "dblock nav-blue")}
-              to={`/invoices/${invoice.number}`}
-              key={invoice.number}
+    const invoices = getInvoices();
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    return (
+        <div style={{ display: "flex" }}>
+            <nav
+                style={{
+                    borderRight: "solid 1px",
+                    padding: "1rem",
+                }}
             >
-              {invoice.name}
-            </NavLink>
-          ))}
-        </nav>
-        <Outlet />
-      </div>
-    </>
-  );
+                <input
+                    value={searchParams.get("name") || ""}
+                    onChange={(event) => {
+                        const name = event.target.value;
+                        if (name) {
+                            setSearchParams({ name });
+                        } else {
+                            setSearchParams({});
+                        }
+                    }}
+                />
+
+                {invoices
+                .filter((invoice) => {
+                    const name = searchParams.get("name");
+                    if (!name) {
+                        return true;
+                    }
+                    const invoiceName = invoice.name.toLowerCase();
+                    return invoiceName.startsWith(name.toLowerCase());
+                  })
+                .map((invoice) => (
+                    <QueryLink
+                        className={({ isActive }: any) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+                        to={`/invoices/${invoice.number}`}
+                        key={invoice.number}
+                    >
+                        {invoice.name}
+                    </QueryLink>
+                ))}
+            </nav>
+            <Outlet />
+        </div>
+    );
 }
