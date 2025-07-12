b8c22c0b2cb02d8f09077d410cbcbfc13d6fab89
J-Breno <joao.breno85@hotmail.com>
2025-07-12 01:22:56 -0300

    Rota para não encontrado

---
+++ exercicios/tutorial-react-router/src/routes/NotFound/index.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <>
+            <main style={{padding: "1rem"}}>
+              <p>There's noting here!</p>
+            </main>
+        </>
+    );
+}
