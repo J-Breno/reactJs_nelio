917583d5dd81f59bc81671c6119e211b5b065d39
J-Breno <joao.breno85@hotmail.com>
2025-07-13 14:06:00 -0300

    Ex1 end

--- exercicios/rotas-ex1/src/App.tsx
+++ exercicios/rotas-ex1/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
 import Promotion from "./routes/Home/Promotion";
+import Subscription from "./routes/Home/Subscription";
 
 function App() {
   return <>
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}>
           <Route index element={<HomeBody />} />
-          <Route path="promotions" element={<Promotion />} />
+          <Route path="promotion" element={<Promotion />} />
+          <Route path="sub" element={<Subscription />} />
         </Route>
       </Routes>
     </BrowserRouter>
