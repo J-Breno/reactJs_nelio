ed0d8591c798b0a84ddb6bf0547d2969b767f320
J-Breno <joao.breno85@hotmail.com>
2025-07-13 13:10:21 -0300

    Layout

--- exercicios/rotas-ex1/src/components/Header/index.tsx
+++ exercicios/rotas-ex1/src/components/Header/index.tsx
@@ -1,11 +1,13 @@
+import "./styles.css";
+
 export default function Header() {
-    return(
-        <>
-        <header>
-            <div>
-                <h1>MeuSite</h1>
-            </div>
-        </header>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <div className="container">
+          <h1 className="site-name">MeuSite</h1>
+        </div>
+      </header>
+    </>
+  );
 }
