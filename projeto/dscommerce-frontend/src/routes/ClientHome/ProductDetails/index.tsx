71e5b54dfd5f2578c8f920c523cf055b4b68f6e7
J-Breno <joao.breno85@hotmail.com>
2025-07-13 17:07:45 -0300

    Capturando erro da requisição com catch

--- projeto/dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ projeto/dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 import * as productService from '../../../services/product-service'
@@ -12,6 +12,8 @@ export default function ProductDetails() {
 
   const params = useParams();
 
+  const navigate = useNavigate();
+
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(()=> {
@@ -19,13 +21,16 @@ export default function ProductDetails() {
     .then(response => {
       setProduct(response.data)
     })
+    .catch(() => {
+      navigate("/")
+    });
   }, [])
 
   return (
     <>
       <main>
         <section id="product-details-section" className="dsc-container">
-          {product && <ProductDetailsCard product={product} />}
+         {product && <ProductDetailsCard product={product} />}
           <div className="dsc-btn-page-container">
             <ButtonPrimary text="Comprar" />
             <Link to={"/"}>
