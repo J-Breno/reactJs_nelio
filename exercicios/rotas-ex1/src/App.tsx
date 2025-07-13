cd5378fcb04a99ad3956706c1c53683807c8ede2
J-Breno <joao.breno85@hotmail.com>
2025-07-13 12:49:18 -0300

    Basic routes structure

--- exercicios/rotas-ex1/src/App.tsx
+++ exercicios/rotas-ex1/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
+import Promotion from "./routes/Home/Promotion";
+
 function App() {
-  return <></>;
+  return <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomeBody />} />
+          <Route path="promotions" element={<Promotion />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>;
 }
 
 export default App;
