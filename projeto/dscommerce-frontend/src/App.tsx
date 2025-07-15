7a077ba48a297ded367a2976fe4915ccab1b4840
J-Breno <joao.breno85@hotmail.com>
2025-07-15 18:24:53 -0300

    Registrando pedido e fazendo ajustes finais

--- projeto/dscommerce-frontend/src/App.tsx
+++ projeto/dscommerce-frontend/src/App.tsx
@@ -52,7 +52,11 @@ function App() {
               <Route path="login" element={<Login />} />
               <Route
                 path="confirmation/:orderId"
-                element={<Confirmation />}
+                element={
+                  <PrivateRoute>
+                    <Confirmation />
+                  </PrivateRoute>
+                }
               />
             </Route>
             <Route
